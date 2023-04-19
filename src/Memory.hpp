12304fdb922d67fea30f1e78d68afa3a59247ca9
Jimmy Givans <owuorjimmygivansomondi@gmail.com>
2023-04-19 18:59:33 +0300

    Refactor - extract the functionality of loadFontSet() to a free function
    This is so that we can test this function easily

--- src/Memory.hpp
+++ src/Memory.hpp
@@ -4,11 +4,26 @@
 #include <array>
 #include <cstdint>
 
+#include <gsl/assert>
+
 #include "FontSet.hpp"
 
 namespace chip8
 {
     inline constexpr unsigned int FontSetStartAddress = 0x50;
+
+    /// \brief Load the font set into the memory buffer
+    /// \tparam size The size of the memory buffer
+    /// \param[in] memoryBuffer The memory buffer
+    template <std::size_t size>
+    constexpr void loadFontSet(std::array<uint8_t, size>& memoryBuffer) noexcept
+    {
+        Expects(memoryBuffer.size() > FontSetSize);
+
+        for (std::size_t i = 0; i < FontSetSize; ++i) {
+            memoryBuffer[FontSetStartAddress + i] = FontSet[i];
+        }
+    }
 
     class Memory
     {
@@ -30,9 +45,7 @@ namespace chip8
 
     constexpr void Memory::loadFontSet() noexcept
     {
-        for (std::size_t i = 0; i < FontSetSize; ++i) {
-            m_memory[FontSetStartAddress + i] = FontSet[i];
-        }
+        chip8::loadFontSet(m_memory);
     }
 }
 
