d0f59e6bf67c1e982d49162c522a0a2d42f4ad94
Jimmy Givans <owuorjimmygivansomondi@gmail.com>
2023-04-20 15:07:30 +0300

    Implement top() and pop() for the stack

--- src/Stack.cpp
+++ src/Stack.cpp
@@ -1,1 +1,19 @@
 #include "Stack.hpp"
+
+namespace chip8
+{
+    void Stack::pop()
+    {
+        stack.pop();
+    }
+
+    std::uint16_t const& Stack::top() const
+    {
+        return stack.top();
+    }
+
+    std::uint16_t& Stack::top()
+    {
+        return stack.top();
+    }
+}
--- src/Stack.hpp
+++ src/Stack.hpp
@@ -13,6 +13,17 @@ namespace chip8
         std::stack<std::uint16_t, std::array<std::uint16_t, 16>> stack {};
 
         Stack() noexcept = default;
+
+        /// \brief Remove the top element from the stack
+        void pop();
+
+        /// \brief Get the element at the top of the stack
+        /// \returns A const reference to the most-recently pushed element
+        std::uint16_t const& top() const;
+
+        /// \brief Get the element at the top of the stack
+        /// \returns A reference to the most-recently pushed element
+        std::uint16_t& top();
     };
 }
 
