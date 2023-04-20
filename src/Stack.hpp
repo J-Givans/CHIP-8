1d89063f3d046bad7a6c7668647ef33869b9e937
Jimmy Givans <owuorjimmygivansomondi@gmail.com>
2023-04-20 15:41:57 +0300

    Refactor - use std::vector as the underlying container for std::stack

--- src/Stack.hpp
+++ src/Stack.hpp
@@ -2,7 +2,7 @@
 #define STACK_HPP
 
 #include <stack>
-#include <array>
+#include <vector>
 #include <cstdint>
 
 namespace chip8
@@ -10,7 +10,7 @@ namespace chip8
     class Stack
     {
     public:
-        std::stack<std::uint16_t, std::array<std::uint16_t, 16>> stack {};
+        std::stack<std::uint16_t, std::vector<std::uint16_t>> stack { std::vector<std::uint16_t>(16) };
 
         Stack() noexcept = default;
 
