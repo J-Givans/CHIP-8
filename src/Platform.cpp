b1408316bfd01536a78e4aa5de4a47e9303d96c3
Jimmy Givans <owuorjimmygivansomondi@gmail.com>
2023-04-25 17:18:50 +0300

    Add constructor for Platform class
    This is used to initialise the window to which components will be rendered

--- src/Platform.cpp
+++ src/Platform.cpp
@@ -1,1 +1,12 @@
 #include "Platform.hpp"
+
+#include <SFML/Window/VideoMode.hpp>
+#include <SFML/System/Vector2.hpp>
+
+namespace chip8
+{
+    Platform::Platform(std::string const& title, unsigned windowWidth, unsigned windowHeight)
+    : window_(sf::VideoMode(windowWidth, windowHeight), title)
+    {
+    }
+}
--- src/Platform.hpp
+++ src/Platform.hpp
@@ -3,10 +3,20 @@
 
 #include <SFML/Graphics/RenderWindow.hpp>
 
+#include <cstdint>
+#include <string>
+
 namespace chip8
 {
     class Platform
     {
+    public:
+        /// \brief Default constructor
+        /// \details Create a new render window with the given dimensions and title
+        /// \param[in] title The title of the window
+        /// \param[in] windowWidth, windowHeight The dimensions of the window
+        Platform(std::string const& title, unsigned windowWidth, unsigned windowHeight);
+
     private:
         sf::RenderWindow window_;
     };
