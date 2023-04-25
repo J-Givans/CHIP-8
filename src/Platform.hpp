5eb87a492655f3a2d699e47e841adf49dc7dc433
Jimmy Givans <owuorjimmygivansomondi@gmail.com>
2023-04-25 13:22:51 +0300

    Initial commit

---
+++ src/Platform.hpp
@@ -0,0 +1,15 @@
+#ifndef PLATFORM_HPP
+#define PLATFORM_HPP
+
+#include <SFML/Graphics/RenderWindow.hpp>
+
+namespace chip8
+{
+    class Platform
+    {
+    private:
+        sf::RenderWindow window_;
+    };
+}
+
+#endif
