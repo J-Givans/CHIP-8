684f32ecabd430086e451a2d1581e02329011c53
Jimmy Givans <owuorjimmygivansomondi@gmail.com>
2023-04-26 13:41:18 +0300

    Docs - add comments to processInput function

--- src/Platform.hpp
+++ src/Platform.hpp
@@ -2,6 +2,8 @@
 #define PLATFORM_HPP
 
 #include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics/Texture.hpp>
+#include <SFML/Graphics/RenderTexture.hpp>
 
 #include <cstdint>
 #include <string>
@@ -18,6 +20,8 @@ namespace chip8
         /// \param[in] windowWidth, windowHeight The dimensions of the window
         Platform(std::string const& title, unsigned windowWidth, unsigned windowHeight);
 
+        /// \brief Process any input from the user
+        /// \param[in] keys A set of possible key inputs
         void processInput(std::array<uint8_t, 16>& keys);
 
     private:
