df2400fa7fdfe3bf4dfaa7f3e1ee2974ba58923c
Jimmy Givans <owuorjimmygivansomondi@gmail.com>
2023-04-29 12:18:47 +0300

    Add cmd-line args to be used in setting up the emulator

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
 #include <cstdlib>
+#include <iostream>
+#include <string>
 
-int main()
+int main(int argc, char const* const argv[])
 {
+    if (argc != 4) {
+        std::cerr << "Usage: " << argv[0] << " <Scale> <Delay> <ROM>\n";
+        std::exit(EXIT_FAILURE);
+    }
+
+    [[maybe_unused]] int const videoScale { std::stoi(argv[1]) };
+    [[maybe_unused]] int const cycleDelay { std::stoi(argv[2]) };
+    [[maybe_unused]] char const* const filename { argv[3] };
+
     return EXIT_SUCCESS;
 }
